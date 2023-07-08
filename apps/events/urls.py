45fc94876c0be7005e17fd47f12020ffa98f6c86
Pavel <111009132+sound1ust@users.noreply.github.com>
2023-07-08 13:26:13 +0300

    views and broken urls (#3)
    * views and broken urls
    
    * crud views
    
    * supers w no params, int: -> str: in urls

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.urls import include, path
+
+from .views import EventCreation, EventListing, EventDetail
+
+
+urlpatterns = [
+    path('/', EventListing.as_view(), name='index'),
+    # path('<str:category_id>', EventListing.as_view(), name='category'),
+    path('<str:event_id>/', EventDetail.as_view(), name='detail'),
+    path('create/', EventCreation.as_view(), name='event_creation'),
+    path('<str:event_id>/update/', EventCreation.as_view(), name='event_creation'),
+]
