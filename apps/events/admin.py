c2f5e7cb2a6670232613171de2d0c3d6e42fd206
Vadim <47281257+GithubHungry@users.noreply.github.com>
2023-07-08 12:40:43 +0300

    register models (#2)
    Co-authored-by: Vadim Bardier <vadim@Vadims-MacBook-Pro.local>

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Categories, Event
+
 
 # Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "category", "address", "description", "start_date", "end_date"]
+    list_filter = ["name", "category"]
+    search_fields = ["name", "category"]
+    ordering = ["start_date", "name"]
+
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+    ordering = ["name"]
