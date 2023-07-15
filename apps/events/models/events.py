a130a1a2807eadf5ccd8038107b1fd5637a43d5e
Pavel <pavelsoll2@gmail.com>
2023-07-15 08:27:04 +0300

    base frontend

--- apps/events/models/events.py
+++ apps/events/models/events.py
@@ -2,7 +2,6 @@ from django.db.models import fields
 from django.contrib.auth.models import User
 
 from django.db import models
-from django.urls import reverse
 
 from apps.core.models import BaseModel
 
@@ -11,17 +10,14 @@ class Categories(BaseModel):
     name = fields.CharField(max_length=250)
 
     def __str__(self):
-        return f"{self.name} {self.id}"
-
-    def get_absolute_url(self):
-        return reverse('events:category', args=[str(self.id)])
+        return self.name
 
 
 class Event(BaseModel):
     category = models.ForeignKey(
         Categories, on_delete=models.CASCADE, related_name="category_events", null=True, blank=True
     )
-    name = fields.CharField(max_length=250, null=True, blank=True)
+    name = fields.CharField(max_length=250, unique=True, null=True, blank=True)
     address = fields.CharField(max_length=250, null=True, blank=True)
     description = fields.TextField(max_length=250, null=True, blank=True)
     start_date = fields.DateTimeField(null=True, blank=True)
@@ -32,7 +28,7 @@ class Event(BaseModel):
         ordering = ["start_date"]
 
     def __str__(self):
-        return f"{self.name} {self.id}"
+        return self.name
 
     def get_absolute_url(self):
-        return reverse('events:detail', args=[str(self.id)])
+        return f"/events/{self.id}/"
