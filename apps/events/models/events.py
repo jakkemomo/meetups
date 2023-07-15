a12fef315008e8e93f7b4e4d5691694d4f3cbb74
sytyy00 <sytyy>
2023-07-15 12:31:34 +0300

    [ADD] image field

--- apps/events/models/events.py
+++ apps/events/models/events.py
@@ -1,28 +1,33 @@
 from django.db.models import fields
-from django.contrib.auth.models import User
-
 from django.db import models
+from django.conf import settings
 
-from apps.core.models import BaseModel
+from apps.core.models import AbstractBaseModel
+from apps.events.utils import events_image_upload_path
+
+user_model = settings.AUTH_USER_MODEL
 
 
-class Categories(BaseModel):
+class Categories(AbstractBaseModel):
     name = fields.CharField(max_length=250)
 
     def __str__(self):
         return self.name
 
 
-class Event(BaseModel):
+class Event(AbstractBaseModel):
     category = models.ForeignKey(
         Categories, on_delete=models.CASCADE, related_name="category_events", null=True, blank=True
     )
     name = fields.CharField(max_length=250, unique=True, null=True, blank=True)
     address = fields.CharField(max_length=250, null=True, blank=True)
     description = fields.TextField(max_length=250, null=True, blank=True)
+    image = models.ImageField(upload_to=events_image_upload_path, null=True, blank=True)
+
     start_date = fields.DateTimeField(null=True, blank=True)
     end_date = fields.DateTimeField(null=True, blank=True)
-    users = models.ManyToManyField(User, null=True, blank=True)
+
+    participants = models.ManyToManyField(user_model, blank=True)
 
     class Meta:
         ordering = ["start_date"]
