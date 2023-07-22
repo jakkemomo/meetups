3c5549aa98bfad4dfa16ebc9a943bdcff5f12dca
Alexander Romanovich <alexolikov@gmail.com>
2023-07-22 13:43:39 +0300

    Fixes of authorization after review

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,16 +3,21 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views import generic
 
+from django.contrib.auth import login as auth_login
+
 
 class CoreSignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("core:login")
+    success_url = reverse_lazy("events:event_map")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        auth_login(self.request, self.request.user)
+        return super(CoreSignUpView, self).form_valid(form)
 
 
 class CoreLoginView(LoginView):
-    next = reverse_lazy("events:event_map")
-
     def get_success_url(self):
         url = self.get_redirect_url()
         if url:
@@ -22,8 +27,6 @@ class CoreLoginView(LoginView):
 
 
 class CoreLogoutView(LogoutView):
-    next = reverse_lazy("events:event_map")
-
     def get_success_url(self):
         url = self.get_redirect_url()
         if url:
