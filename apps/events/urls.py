27e34979593210417afb7e6105a2bb4b7e4b3560
Alexander Romanovich <alexolikov@gmail.com>
2023-08-12 09:20:01 +0300

    Fixed edit view, fixed form validation errors, added geolocation.js

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -10,13 +10,10 @@ urlpatterns = [
     path("list/", EventListing.as_view(), name="event_list"),
     path("create/", EventCreation.as_view(), name="event_creation"),
     path("<str:pk>/", EventDetail.as_view(), name="event_detail"),
-
-    path("<str:pk>/update/", EventEdition.as_view(), name="event_creation"),
+    path("<str:pk>/update/", EventEdition.as_view(), name="event_edition"),
     path("<str:pk>/delete/", EventDeletion.as_view(), name="event_deletion"),
-
     path("events/<int:event_id>/register/", RegisterToEvent.as_view(), name="register_to_event"),
     path("events/<int:event_id>/leave/", LeaveFromEvent.as_view(), name="leave_from_event"),
-
     path("events/<int:event_id>/rate/<int:value>", RateEvent.as_view(), name="rate_an_event"),
     path("events/<int:event_id>/remove_rating/", RateEvent.as_view(), name="remove_rating"),
 ]
