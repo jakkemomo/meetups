e9b9e35bb2c5c79f6e234e8fa044a1470ae8f08a
evgeny <evgoravskiy@gmail.com>
2023-12-23 12:30:56 +0300

    Added permissions and serializations for UserRating

---
+++ apps/profiles/permissions/user_ratings.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+from apps.events.permissions.common import is_verified, is_participant
+
+
+class UserRatingPermissions(permissions.BasePermission):
+    """
+    Object-level permission to only verified and participant user to edit another user of the event.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif view.action in ['create', 'update',]:
+            return is_verified(request) and is_participant(request, obj)
+        else:
+            return False
---
+++ apps/profiles/serializers/user_ratings.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+from apps.profiles.models.user_rate import UserRating, User
+
+
+class UserRatingCreateSerializer(serializers.ModelSerializer):
+    value = serializers.IntegerField(min_value=0, max_value=5, default=5)
+
+    class Meta:
+        model = UserRating
+        fields = ['value', 'user_rated', 'comment']
+
+    def create(self, validated_data):
+        user_rated = get_object_or_404(User, id=self.context["view"].kwargs["user_rated_id"])
+        user_rater = self.context["request"].user_rater
+        value = validated_data.pop("value")
+        user_rating_object = UserRating.objects.create(user_rated=user_rated, user_rater=user_rater, value=value)
+
+        return user_rating_object
+
+
+class UserRatingUpdateSerializer(serializers.ModelSerializer):
+    """
+    User's rating serializer for profile viewset
+    """
+    value = serializers.IntegerField(min_value=0, max_value=5, default=5)
+
+    class Meta:
+        model = UserRating
+        fields = ['value', 'user_rated', 'comment']
+
+    def update(self, instance, validated_data):
+        value = validated_data.pop("value")
+        instance.value = value
+        instance.save()
+
+        return instance
+
+
+class UserRatingListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserRating
+        fields = ['value', 'user_rater', 'user_rated', 'comment']
+
+
+class UserRatingRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRating
+        fields = ['value', 'user_rated', 'comment']
