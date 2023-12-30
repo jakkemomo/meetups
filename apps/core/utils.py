86378464178790eaf7df9aa4cb205f9c10bfdcac
pavelkudrautsau <kudrautsau.pavel@gmail.com>
2023-12-30 13:41:07 +0300

    refactoring

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,12 +1,11 @@
 from django.core.files.storage import default_storage
 
 
-def delete_image_if_exists(instance):
+def delete_image_if_exists(instance) -> None:
     image_url = instance.image_url
     if image_url:
         try:
             if default_storage.exists(image_url):
                 default_storage.delete(image_url)
-
         except Exception as exc:
-            return exc
+            raise exc
