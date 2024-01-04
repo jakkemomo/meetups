0dab1f9145168c2e51fdca7c1c944e2f66311ac5
evgeny <evgoravskiy@gmail.com>
2024-01-04 11:18:38 +0300

    Refactored: methods update, create serialization for UserRating

--- apps/profiles/serializers/user_ratings.py
+++ apps/profiles/serializers/user_ratings.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
-from apps.profiles.models.user_rate import UserRating, User
+from apps.profiles.models.user_rate import UserRating
 
 
 class UserRatingCreateSerializer(serializers.ModelSerializer):
+    """
+    User's rating serializers for profile viewset
+    """
     value = serializers.IntegerField(min_value=0, max_value=5, default=5)
 
     class Meta:
@@ -11,33 +13,19 @@ class UserRatingCreateSerializer(serializers.ModelSerializer):
         fields = ['value', 'comment']
 
     def create(self, validated_data):
-        user = get_object_or_404(User, id=self.context["view"].kwargs["user_id"])
-        creator = self.context["request"].user
-        value = validated_data.pop("value")
-        user_rating = UserRating.objects.create(user_rated=user, value=value, created_by=creator)
+        validated_data['created_by_id'] = self.context["request"].user.id
+        validated_data['user_rated_id'] = self.context["view"].kwargs["user_id"]
+        user_rating = UserRating(**validated_data)
+        user_rating.save()
         return user_rating
 
 
 class UserRatingUpdateSerializer(serializers.ModelSerializer):
-    """
-    User's rating serializer for profile viewset
-    """
     value = serializers.IntegerField(min_value=0, max_value=5, default=5)
 
     class Meta:
         model = UserRating
         fields = ['value', 'comment']
-
-    def update(self, instance, validated_data):
-        value = validated_data.pop("value")
-        comment = validated_data.pop("comment")
-        updater = self.context["request"].user
-        instance.value = value
-        instance.comment = comment
-        instance.updated_by = updater
-        instance.save()
-
-        return instance
 
 
 class UserRatingListSerializer(serializers.ModelSerializer):
