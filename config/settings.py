05a901aeb70583e7363c6bd72558af8aa85f936c
Alexander Romanovich <alexolikov@gmail.com>
2024-02-04 14:07:32 +0200

    Added filters and ordering to events

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "drf_yasg",
+    'django_filters',
     "corsheaders",
 ]
 
@@ -177,6 +178,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework_simplejwt.authentication.JWTAuthentication', ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 24,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {
