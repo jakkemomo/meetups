48ff7a5690e048800ab8ae302c7ffcb458b0dd41
Pavel <pavelsoll2@gmail.com>
2024-03-16 15:13:31 +0300

    Followers websockets notifications Added JwtAuthMiddleware, NotificationConsumer and NotificationManager for notifications. Added base receive test. TODO: followers ws tests.

--- apps/profiles/viewsets.py
+++ apps/profiles/viewsets.py
@@ -7,10 +7,9 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from apps.core.utils import delete_image_if_exists
 from apps.events.models import Event
 from apps.events.serializers import EventListSerializer
-from apps.profiles.managers import NotificationManager
+from apps.core.utils import delete_image_if_exists
 from apps.profiles.managers import NotificationManager
 from apps.profiles.models import UserRating, User
 from apps.profiles.models.followers import Follower
