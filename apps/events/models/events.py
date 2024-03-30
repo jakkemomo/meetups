2e9bbec550478d7e2f48ac6f985cefc838ed0047
Alexander Romanovich <alexolikov@gmail.com>
2024-03-30 09:04:01 +0200

    Added event fields

--- apps/events/models/events.py
+++ apps/events/models/events.py
@@ -5,18 +5,28 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import fields
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 from apps.core.models import AbstractBaseModel
 from apps.events.models.categories import Category
 from apps.events.models.rating import Rating
+from apps.events.models.schedule import Schedule
 from apps.events.models.tags import Tag
-from apps.events.utils import Currency, PeriodRepeatability
 from apps.profiles.models.city import City
 
 user_model = settings.AUTH_USER_MODEL
 
 
 class Event(AbstractBaseModel):
+    class Currency(models.TextChoices):
+        BYN = 'BYN', _('Belarusian ruble')
+        USD = 'USD', _('United States dollar')
+        EUR = 'EUR', _('Euro')
+        RUB = 'RUB', _('Russian ruble')
+        PLN = 'PLN', _('Polish zloty')
+        UAH = 'UAH', _('Ukrainian hryvnia')
+        KZT = 'KZT', _('Kazakhstani tenge')
+
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="category_events", null=True, blank=True
     )
@@ -46,12 +56,11 @@ class Event(AbstractBaseModel):
     desired_participants_number = models.PositiveIntegerField(default=1, null=False, blank=False)
     ratings = models.ManyToManyField(user_model, through=Rating, through_fields=("event", "user"))
     repeatable = fields.BooleanField(default=False)
-    repeatability = fields.CharField(choices=[(period.value, period) for period in PeriodRepeatability],
-                                     default=PeriodRepeatability.MONTH)
+    schedule = models.ManyToManyField(Schedule, related_name="events", blank=True)
     participants_age = fields.PositiveSmallIntegerField(default=18)
     cost = fields.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     free = fields.BooleanField(default=True)
-    currency = fields.CharField(choices=[(cur.value, cur) for cur in Currency], default=Currency.BYN)
+    currency = fields.CharField(max_length=3, choices=Currency.choices, default=Currency.BYN)
     gallery = ArrayField(models.CharField(max_length=250, null=True, blank=True, default=""), default=list)
 
     class Meta:
