17235dc09da890d9b57d00350b04726581887efd
Alexander Romanovich <alexolikov@gmail.com>
2024-04-02 19:43:46 +0300

    added new run sh script

--- deployments/app/scripts/run.sh
+++ deployments/app/scripts/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-python manage.py migrate --noinput \
-        && python manage.py collectstatic --noinput \
+# Apply migrations to the first database from env variable called POSTGRES_DB
+# Then apply migrations to the second database from env variable called WS_DB
+python manage.py migrate --noinput --database=$POSTGRES_DB \
+  && python manage.py migrate --noinput --database=$WS_DB \
+  && python manage.py collectstatic --noinput \
         && python manage.py runserver 0.0.0.0:8000
