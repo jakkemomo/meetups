0d2eb1eb1fa2faa40bd248e3b9ee55dab58c40fc
Pavel <pavelsoll2@gmail.com>
2024-04-04 10:45:20 +0300

    WS hotfix Added Origin validator that checks Origin header. Added Allowed headers (origins): locals and our frontend.

--- config/asgi.py
+++ config/asgi.py
@@ -1,23 +1,26 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
+from channels.security.websocket import OriginValidator
 
 from django.core.asgi import get_asgi_application
 
 from apps.core.middlewares import JwtAuthMiddlewareStack
+from config.settings import WS_ALLOWED_ORIGINS
 
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-
 from apps.profiles.routers import websocket_urlpatterns
-
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': JwtAuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    'websocket': OriginValidator(
+        JwtAuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
+        allowed_origins=WS_ALLOWED_ORIGINS,
     )
 })
