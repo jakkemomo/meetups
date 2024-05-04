98fc8621368e509a1fe70d17391cb3c668a3fbd1
pavelkudrautsau <kudrautsau.pavel@gmail.com>
2024-05-04 13:20:32 +0300

    Add category_image_field

--- apps/events/models/categories.py
+++ apps/events/models/categories.py
@@ -6,6 +6,7 @@ from apps.core.models import AbstractBaseModel
 
 class Category(AbstractBaseModel):
     name = fields.CharField(max_length=250, unique=True)
+    image_url = fields.CharField(max_length=500, blank=True, null=True)  # image
 
     def __str__(self):
         return self.name
