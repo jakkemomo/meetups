412f11e882186d76c5233d2bbcaa3a110c600288
evgeny <evgoravskiy@gmail.com>
2024-05-16 15:39:44 +0300

    Added: filters to FollowerViewSet

--- apps/profiles/filters/filters_followers.py
+++ apps/profiles/filters/filters_followers.py
@@ -25,24 +25,25 @@ class UserFollowersFilter(filters.FilterSet):
             'city_in',
         ]
 
-        class UserFollowsFilter(filters.FilterSet):
-            """ Setting filter of users who is current user follows """
-            username = filters.CharFilter(lookup_expr='exact', field_name='user__username')
-            username_contains = filters.CharFilter(lookup_expr='icontains', field_name='user__username')
-            age = filters.NumberFilter(lookup_expr='exact', field_name='user__age')
-            age__gte = filters.NumberFilter(lookup_expr='gte', field_name='user__age')
-            age__lte = filters.NumberFilter(lookup_expr='lte', field_name='user__age')
-            city = filters.CharFilter(lookup_expr='exact', field_name='user__city')
-            city_in = filters.CharFilter(lookup_expr='in', field_name='user__city')
 
-            class Meta:
-                model = Follower
-                fields = [
-                    'username',
-                    'username_contains',
-                    'age',
-                    'age__gte',
-                    'age__lte',
-                    'city',
-                    'city_in',
-                ]
+class UserFollowsFilter(filters.FilterSet):
+    """ Setting filter of users who is current user follows """
+    username = filters.CharFilter(lookup_expr='exact', field_name='user__username')
+    username_contains = filters.CharFilter(lookup_expr='icontains', field_name='user__username')
+    age = filters.NumberFilter(lookup_expr='exact', field_name='user__age')
+    age__gte = filters.NumberFilter(lookup_expr='gte', field_name='user__age')
+    age__lte = filters.NumberFilter(lookup_expr='lte', field_name='user__age')
+    city = filters.CharFilter(lookup_expr='exact', field_name='user__city')
+    city_in = filters.CharFilter(lookup_expr='in', field_name='user__city')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'username',
+            'username_contains',
+            'age',
+            'age__gte',
+            'age__lte',
+            'city',
+            'city_in',
+        ]
