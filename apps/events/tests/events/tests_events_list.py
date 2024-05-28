3944f115b4c67d90b19131ffdc7739dc5be6dc82
Yauheni Harauski <goravsky@tut.by>
2024-05-28 17:10:54 +0300

    Update tests_events_list.py
    Refactored test_event_get_filtered_by_geo()

--- apps/events/tests/events/tests_events_list.py
+++ apps/events/tests/events/tests_events_list.py
@@ -123,14 +123,12 @@ def test_event_get_authorised_not_found(api_client, user, event_private):
 
 @pytest.mark.django_db
 def test_event_get_filtered_by_geo(api_client, hundred_events):
-    # response_minsk = api_client.get(reverse(EVENTS_LIST_URL),
-    #                                 kwargs={'min_lat': '53.829', 'max_lat': '53.973', 'min_lng': '27.401',
-    #                                         'max_lng': '27.703', })
-    response_minsk = api_client.get('http://localhost:8000/api/v1/events/?min_lat=53.829&max_lat=53.973&min_lng=27.401&max_lng=27.703')
-    # response_moscow = api_client.get(reverse(EVENTS_LIST_URL),
-    #                                  kwargs={'min_lat': '55.569', 'max_lat': '55.916', 'min_lng': '37.360',
-    #                                          'max_lng': '37.859', })
-    response_moscow = api_client.get('http://localhost:8000/api/v1/events/?min_lat=55.569&max_lat=55.916&min_lng=37.360&max_lng=37.859')
+    response_minsk = api_client.get(reverse(EVENTS_LIST_URL),
+                                    data={'min_lat': '53.829', 'max_lat': '53.973', 'min_lng': '27.401',
+                                            'max_lng': '27.703', })
+      response_moscow = api_client.get(reverse(EVENTS_LIST_URL),
+                                     data={'min_lat': '55.569', 'max_lat': '55.916', 'min_lng': '37.360',
+                                             'max_lng': '37.859', })
     assert response_minsk.status_code == 200
     assert response_moscow.status_code == 200
     assert response_minsk.data.get('count') == 100
