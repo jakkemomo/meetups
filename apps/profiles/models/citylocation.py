41cb41498486896daf256c0b981b2a7aa7903428
evgeny <evgoravskiy@gmail.com>
2024-06-03 17:27:09 +0300

    Refactored: Squashedmigrations

--- apps/profiles/models/citylocation.py
+++ apps/profiles/models/citylocation.py
@@ -6,50 +6,6 @@ from apps.core.models import AbstractBaseModel
 
 
 class CityLocation(AbstractBaseModel):
-    """
-    Object for storing place data.
-    REST Resources of places from Google MAPS Platform Places API. Response JSON-represented:
-        {
-            "id" : "ChIJ02oeW9PP20YR2XC13VO4YQs"
-            "location" : {
-                "latitude" : 53.902284
-                "longitude" : 27.561831
-            }
-            "viewport" : {
-                "low" : {
-                    "latitude" : 53.82427
-                    "longitude" : 27.38909
-                }
-                "high" : {
-                    "latitude" : 53.97800
-                    "longitude" : 27.76125
-                }
-            }
-        }
-    Resources of places from Yandex Maps Geocode API. Response JSON-represented:
-        {
-          "response": {
-            "GeoObjectCollection": {
-              "featureMember": [
-                {
-                  "GeoObject": {
-                    "metaDataProperty": {...},
-                    "boundedBy": {
-                      "Envelope": {
-                        "lowerCorner": "27.38909, 53.82427",
-                        "upperCorner": "27.76125 53.97800"
-                      }
-                    },
-                    "Point": {
-                      "pos": "27.561831 53.902284"
-                    }
-                  }
-                }
-              ]
-            }
-          }
-        }
-    """
     location = PointField(default=Point(27.561831, 53.902284), unique=True)
     # Yandex : response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope.lowerCorner
     # Google : viewport.low.latitude&longitude
