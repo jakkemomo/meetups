fdd836827fdf2b62a68142b06275396af34d7fb9
evgeny <evgoravskiy@gmail.com>
2024-06-06 11:24:23 +0300

    Refactored: added place_id field to CityLocation

--- apps/profiles/models/citylocation.py
+++ apps/profiles/models/citylocation.py
@@ -6,6 +6,7 @@ from apps.core.models import AbstractBaseModel
 
 
 class CityLocation(AbstractBaseModel):
+    place_id = models.CharField(max_length=255, unique=True)
     location = PointField(default=Point(27.561831, 53.902284), unique=True)
     # Yandex : response.GeoObjectCollection.featureMember[0].GeoObject.boundedBy.Envelope.lowerCorner
     # Google : viewport.low.latitude&longitude
