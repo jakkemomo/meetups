3e29dfc1fc0458e5ef5a56a15bc4de4a1e269740
evgeny <evgoravskiy@gmail.com>
2024-07-20 09:22:06 +0300

    Refactored: view only get list endpoint

--- apps/events/viewsets/city.py
+++ apps/events/viewsets/city.py
@@ -1,16 +1,16 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
 
 from apps.events.models.city import City
 from apps.events.serializers.city import CityListSerializer
 
 
-class CityViewSet(viewsets.ModelViewSet):
+class CityViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """
     A simple ViewSet for viewing City data.
     """
-    model = City
+
     permission_classes = [AllowAny]
     queryset = City.objects.all()
     serializer_class = CityListSerializer
