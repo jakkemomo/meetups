348b8f55f2894d5dfe5164a26560461b08452416
Степан Суппес <suppes214@gmail.com>
2024-08-11 19:14:43 +0300

    1

--- apps/core/views/tokens.py
+++ apps/core/views/tokens.py
@@ -1,6 +1,5 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
     TokenObtainPairView,
@@ -45,10 +44,4 @@ class DecoratedTokenObtainPairView(TokenObtainPairView):
         responses={status.HTTP_200_OK: TokenObtainPairResponseSerializer}, tags=["auth"]
     )
     def post(self, request, *args, **kwargs):
-        if request.user.is_email_verified:
-            return super().post(request, *args, **kwargs)
-        else:
-            return Response(
-                data={"user.is_email_verified": "False, but must be True"},
-                status=status.HTTP_403_FORBIDDEN,
-            )
+        return super().post(request, *args, **kwargs)
