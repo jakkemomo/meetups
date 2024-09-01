e496f614a02212665795d1266de9fdb281bd02a8
Alexander <42062023+jakkemomo@users.noreply.github.com>
2024-09-01 10:37:44 +0300

    Added search by chat participants, changed chat participants to use pagination (#233)
    * Added create_at to chats
    
    * Added freezegun
    
    * Fixed tests
    
    * Added delete many messages endpoint (#229)
    
    * Fixed ws allowed origins to work with mevent io only
    
    * Added unread counter to chats
    
    * Added mark as read functionality
    
    * Added chats migration
    
    * Updated description
    
    * Linted migration
    
    * Added tests to check unread messages
    
    * Lint
    
    * Added search by message in the chat
    
    * Added search by chat participants
    
    * Linter

--- apps/chats/urls.py
+++ apps/chats/urls.py
@@ -1,13 +1,22 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .viewsets import ChatViewSet, ChatMessagesViewSet, DirectChatViewSet, MessageViewSet
+from .viewsets import (
+    ChatViewSet,
+    ChatMessagesViewSet,
+    ChatParticipantsViewSet,
+    DirectChatViewSet,
+    MessageViewSet,
+)
+
 
 app_name = "chats"
 
 router = routers.SimpleRouter()
 router.register("chats", ChatViewSet, basename="Chats")
 router.register("chats", ChatMessagesViewSet, basename="ChatMessages")
+router.register("chats", ChatParticipantsViewSet, basename="ChatParticipants")
+
 router.register("messages", MessageViewSet, basename="Messages")
 router.register("chats/users", DirectChatViewSet, basename="Directs")
 urlpatterns = [path("api/v1/", include(router.urls))]
