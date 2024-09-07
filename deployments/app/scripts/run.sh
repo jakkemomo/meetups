5b1f8dd1896f16be3ccb6afd7b872dcbdb358a2c
Alexander Romanovich <alexolikov@gmail.com>
2024-09-07 16:09:17 +0300

    Added make migrations to run sh in order to update third party apps
    (cherry picked from commit 9230c4d3f0a7ccc444bc2e8ec6327c3ff6e0e1bc)

--- deployments/app/scripts/run.sh
+++ deployments/app/scripts/run.sh
@@ -2,7 +2,8 @@
 
 # Apply migrations to the first database from env variable called POSTGRES_DB
 # Then apply migrations to the second database from env variable called WS_DB
-python manage.py migrate --noinput --database=channels_postgres \
+python manage.py makemigrations --noinput\
+  &&python manage.py migrate --noinput --database=channels_postgres \
   && python manage.py migrate --noinput \
   && python manage.py sync_translation_fields --noinput \
   && python manage.py update_translation_fields \
